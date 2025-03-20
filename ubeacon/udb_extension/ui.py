11f4be1283bd557948d0933bc2b636f2ee1d7f52
Finn Grimwood <fgrimwood@undo.io>
2025-03-20 15:27:24 +0000

    Fix Python TUI source code error message
    If the Python source TUI window couldn't find the source file, then it
    would print an error message to the terminal. This change fixes that.

--- ubeacon/udb_extension/ui.py
+++ ubeacon/udb_extension/ui.py
@@ -9,22 +9,26 @@ from . import ubeacon, tui_windows
 @tui_windows.register_window("python-source")
 class PythonSourceWindow(tui_windows.ScrollableWindow):
     title = "Python Source"
+    no_src_msg = "No source code available"
 
     def get_content(self):
-        if len(ubeacon.backtrace) == 0:
-            return "No source code available"
+        try:
+            if len(ubeacon.backtrace) == 0:
+                return self.no_src_msg
 
-        frame = ubeacon.backtrace.frames[0]
-        filename = frame.file_name
-        line = frame.line
-        lines = ubeacon.get_source_file_content(filename, line_nos=True, highlight=True).split("\n")
-        prefixed_lines = [(" > " if i == line else "   ") + l for i, l in enumerate(lines, start=1)]
+            frame = ubeacon.backtrace.frames[0]
+            filename = frame.file_name
+            line = frame.line
+            lines = ubeacon.get_source_file_content(filename, line_nos=True, highlight=True).split("\n")
+            prefixed_lines = [(" > " if i == line else "   ") + l for i, l in enumerate(lines, start=1)]
 
-        # Set vertical scroll offset to center the current line
-        half_window_height = self._tui_window.height // 2
-        self.vscroll_offset = line - half_window_height
+            # Set vertical scroll offset to center the current line
+            half_window_height = self._tui_window.height // 2
+            self.vscroll_offset = line - half_window_height
 
-        return "\n".join(prefixed_lines)
+            return "\n".join(prefixed_lines)
+        except Exception:
+            return self.no_src_msg
 
 
 @tui_windows.register_window("python-backtrace")
